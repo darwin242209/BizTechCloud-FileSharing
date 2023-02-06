bf15c20d32895aa1744c96a245bcc585f6774ee3
darwin242209 <115344849+darwin242209@users.noreply.github.com>
2023-02-07 00:56:35 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🔒 Exit", callback_data = "close")
                 ]
             ]
         )
